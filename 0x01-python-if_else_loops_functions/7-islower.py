0b0de9f31945f5aa71753895f551fb545a114fa9
Adaobitech <adaobiezenwelu@gmail.com>
2022-12-12 05:15:56 -0800

    Task 7 Lowercase

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(d):
     ascii_num = ord(d)
-    if ascii_num < 97 and ascii_num = 122:
+    if ascii_num < 97 and ascii_num <= 122:
         return true
     return false
