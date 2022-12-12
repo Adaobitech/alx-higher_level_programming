e75b725925a8c00da14c9e4f969be878a471c16c
Adaobitech <adaobiezenwelu@gmail.com>
2022-12-12 04:52:20 -0800

    Task 5 Combo

--- 0x01-python-if_else_loops_functions/test.py
+++ 0x01-python-if_else_loops_functions/test.py
@@ -3,3 +3,13 @@ d = 67
 print(hex(d))
 for n in range(0, 99):
     print("{} =  {}".format(n, hex(n)))
+
+for n in range(0, 99):
+    print(n, end=", ")
+
+
+for number in range(100):
+    if (number != 99):
+        print("{}{}".format(int(number / 10), number % 10), end=', ')
+    else:
+        print("{}{}".format(int(number / 10), number % 10))
