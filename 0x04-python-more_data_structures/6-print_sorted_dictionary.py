eee63130489f312eb1f48e73493ea4eca2b55ecf
Adaobitech <adaobiezenwelu@gmail.com>
2023-02-22 03:23:49 -0800

    Task 6 sorted Dictionary

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     mydict = list(a_dictionary.keys())
+    sorted_dict = {i: a_dictionary[i] for i in mydict}
 
     return (mydict.sort())
