a75c4e2e1660324c261d2330e3e9237ad1bf885f
Adaobitech <adaobiezenwelu@gmail.com>
2024-03-25 04:24:00 -0700

    Python Network

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send a GET request to a given URL with a header variable.
-curl -sH "X-HolbertonSchool-User-Id: 98" "${1}"
+[ -z "$1" ] && { echo "Usage: $0 <URL>"; exit 1; }; URL="$1"; HEADER="X-School-User-Id: 98"; curl -sSL -H "$HEADER" "$URL"
