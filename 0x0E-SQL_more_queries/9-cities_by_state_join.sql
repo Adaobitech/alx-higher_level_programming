afb8d12bbf123f4188d91472c11830bb3b6377be
Adaobitech <adaobiezenwelu@gmail.com>
2024-03-26 02:33:37 -0700

    Task 9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- List all cities in the database 'hbtn_0d_usa'
 -- Each record should display, cities.id, cities.name, states.name
-SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.states_id = states.id
+SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
